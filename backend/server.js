f6c0b638994fab1b98a1fdef2360ba2df64937b9
Nalin Pradhan <64825639+NalinPradhan@users.noreply.github.com>
2024-09-19 03:55:52 +0000

    saving data

--- backend/server.js
+++ backend/server.js
@@ -4,15 +4,20 @@ const mongoose = require('mongoose');
 const Cv = require('./model');
 
 const app = express();
-const dbURI= 'mongodb+srv://nalinforodin:Kansasnalin1@theodinproject.w97oi.mongodb.net/cv?retryWrites=true&w=majority&appName=theodinproject';
+const dbURI = 'mongodb+srv://nalinforodin:Kansasnalin1@theodinproject.w97oi.mongodb.net/cv?retryWrites=true&w=majority&appName=theodinproject';
 mongoose.connect(dbURI).then((result) => {
     console.log('connected to db');
-    app.listen(3000) ;
-    
+    app.listen(3000);
+
 }).catch((err) => console.log(err));
-app.get('/cv', (req, res) => {
-    Cv.find().then((result) => {
-        console.log(result);
+app.get('/add-cv', (req, res) => {
+    const newCv = new Cv({
+        name: 'Aprina Shah',
+        email: 'aprina@gmial.com',
+        phone: '1234567890'
+    });
+    newCv.save().then((result) => {
+        console.log('cv added');
         res.send(result);
     }).catch((err) => {
         console.log(err);
